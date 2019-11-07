e5e6ef7b26175b099cf5f9f7ca21c4e45a326282
Victor Colin Amador <vicolina@microsoft.com>
2019-11-06 16:20:37 -0800

    Changed package name for a couple test files under the app module.

--- app/src/androidTest/java/com/azure/data/android/benchmark/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/azure/data/android/benchmark/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.azure.data.android.benchmarkdesugaring;
+package com.azure.data.android.benchmark;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.azure.data.android.benchmarkdesugaring", appContext.getPackageName());
+        assertEquals("com.azure.data.android.benchmark", appContext.getPackageName());
     }
 }
